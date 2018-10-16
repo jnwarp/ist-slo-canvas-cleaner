39847aa7e80f207080b6c6a6edac97c5b8e22d05
James Warp <james@jnwarp.com>
2018-10-16 17:05:14 -0400

    Update project with test data

--- cleanup.py
+++ cleanup.py
@@ -6,12 +6,15 @@ Programmed by James Regan (akr5321)
 import pandas as pd
 
 # only read columns printed below
-cols = ['Name', 'Points']
-df = pd.read_excel('canvas.xlsx', usecols=cols)
+cols = ['Student', 'Current Points', 'Final Points']
+df = pd.read_excel('Test data.xlsx', usecols=cols)
+
+# drop first row (with read only tags)
+df = df.drop([0], axis=0)
 
 # remove students below point value
 point_min = 0.5
-df = df[df['Points'] >= point_min]
+df = df[df['Final Points'] >= point_min]
 
 print(df)
 
